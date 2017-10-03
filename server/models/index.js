ee4168339559bb671357ccd127c729e569121065
jenniferneale <jeneale6@gmail.com>
2017-10-03 17:49:37 -0400

    Defined model tables

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,81 @@
 const Sequelize = require('sequelize');
-const {INTEGER, STRING, TEXT, VIRTUAL, ARRAY} = Sequelize;
+const {INTEGER, STRING, TEXT, VIRTUAL, ARRAY, FLOAT} = Sequelize;
 
-const db = new Sequelize('postgres://localhost:5432/wikistack', {logging: false});
+const db = new Sequelize('postgres://localhost:5432/trip-planner', {logging: false});
 
-module.exports = {db};
+
+
+const Hotel = db.define('hotel', {
+    name: {
+        type: STRING,
+        allowNull: false
+    },
+    num_stars: {
+        type: FLOAT,
+        allowNull: false,
+        validate: {
+            max: 5,
+            min:0
+        }
+    },
+    amenities: {
+        type: STRING        
+    }
+});
+
+const Place = db.define('place', {
+    address: {
+        type: STRING,
+        allowNull:false
+    },
+    city: {
+        type: STRING,
+        allowNull: false
+    },
+    state: {
+        type: STRING,
+        allowNull:false
+    },
+    phone: {
+        type: STRING
+    },
+    location: {
+        type: ARRAY(FLOAT),
+        allowNull: false
+    }
+});
+
+const Activity = db.define('activity', {
+    name: {
+        type: STRING,
+        allowNull: false
+    },
+    age_range: {
+        type: STRING,
+        defaultValue: "UNKNOWN. BRING CHILDREN AT YOUR PERIL."
+    }
+});
+
+const Restaurant = db.define('restaurant', {
+    name: {
+        type: STRING,
+        allowNull: false
+    },
+    cuisine: {
+        type: STRING
+    },
+    price: {
+        type: INTEGER,
+        allowNull: false,
+        validate: {
+            max: 5,
+            min: 0
+        }
+    }
+});
+
+Restaurant.belongsTo(Place, { onDelete: 'cascade' });
+Hotel.belongsTo(Place, { onDelete: 'cascade' });
+Activity.belongsTo(Place, { onDelete: 'cascade' });
+
+module.exports = { db, Hotel, Restaurant, Place, Activity };
