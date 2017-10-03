e1dad6f913c8859847cb5e0b39b7c66d17ac4b38
Caryn McCarthy <cmccarthy15@gmail.com>
2017-10-03 18:08:29 -0400

    finished part 3 backend

---
+++ server/api.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+const {db, Hotel, Place, Activity, Restaurant} = require("./models");
+
+
+router.get('/', (req, res, next) => {
+  Promise.all([Hotel.findAll({include: [Place]}), Activity.findAll({include: [Place]}), Restaurant.findAll({include: [Place]})])
+  .then( arr => res.status(200).json(arr))
+  .catch(next);
+});
+
+router.get('/hotels', (req, res, next) => {
+  Hotel.findAll()
+  .then( hotels => {
+    res.status(200).json(hotels);
+  })
+  .catch(next);
+});
+
+router.get('/activities', (req, res, next) => {
+  Activity.findAll()
+  .then( activities => {
+    res.status(200).json(activities);
+  })
+  .catch(next);
+});
+
+router.get('/restaurants', (req, res, next) => {
+  Restaurant.findAll()
+  .then( restaurants => {
+    res.status(200).json(restaurants);
+  })
+  .catch(next);
+});
+
+module.exports = router;
--- server/app.js
+++ server/app.js
@@ -5,13 +5,17 @@ const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-var db = require("./models").db;
+const db = require("./models").db;
+
+const apiRouter = require('./api.js');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('/api', apiRouter);
 
 app.use( (req, res, next) => {
   var err = new Error('Not Found');
