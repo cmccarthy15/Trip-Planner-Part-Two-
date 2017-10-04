4781524fbcb1ad45d940b95c2f4faed9d13c4ab6
jenniferneale <jeneale6@gmail.com>
2017-10-04 12:30:33 -0400

    So shiny; needs refactoring

--- client/index.js
+++ client/index.js
@@ -3,6 +3,9 @@ const buildMarker = require('./marker.js');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY21jY2FydGh5MTUiLCJhIjoiY2o4YnFiN3RyMDBuNjJ3c2Y4dzhkMnIzNyJ9.ebxoBjxotfIr5-5EIsZ1hA';
 
+//const { db, Hotel, Place, Activity, Restaurant } = require("../server/models");
+//const tables = [Hotel, Restaurant, Activity];
+
 const map = new mapboxgl.Map({
   container: 'map',
   center: [-74.009, 40.705], // FullStack coordinates
@@ -15,6 +18,10 @@ marker.addTo(map);
 
 const selectIds = ['hotels-choices', 'restaurants-choices', 'activities-choices'];
 const buttonIds = ['hotels-add', 'restaurants-add', 'activities-add'];
+const itineraryIds = ['hotels-list', 'restaurants-list', 'activities-list'];
+var removeButton = document.createElement("BUTTON");
+removeButton.classList.add("remove-btn");
+removeButton.innerHTML = "x";
 
 fetch('/api/attractions')
 .then(result => result.json())
@@ -30,7 +37,7 @@ function createAndAddOptions(parentId, optionArr){
   const parent = document.getElementById(parentId);
   optionArr.forEach( option => {
     let newNode = document.createElement('OPTION');
-    newNode.value = option.name;
+    newNode.value = option.id;
     newNode.text = option.name;
     parent.appendChild(newNode);
   });
@@ -49,19 +56,117 @@ hotelButton.onclick = () => {
   var hotels = document.getElementById('hotels-choices');
   var selection = hotels.options[hotels.selectedIndex].value;
   console.log('hotel: ', selection);
+  fetch('/api/hotels/' + selection)
+      .then(result => {          
+          return result.json();
+      }).then(data => {
+          console.log(data);
+          //Add to itinerary
+          const parent = document.getElementById(itineraryIds[0]);
+          let divNode = document.createElement('DIV');
+          let pNode = document.createElement('SPAN');
+          pNode.innerHTML = data.name;
+          divNode.appendChild(pNode);
+          let remNode = removeButton.cloneNode(true);
+          divNode.appendChild(remNode);
+          parent.appendChild(divNode);
+          //Add marker
+          var newMarker = buildMarker('hotels', data.place.location);
+          newMarker.addTo(map);
+          map.flyTo({
+              center: data.place.location,
+              zoom: 15,
+              curve: 1.8
+          });
+          remNode.onclick = function () {
+              newMarker.remove();
+              this.parentElement.remove();
+              map.flyTo({
+                  center: [-74.009, 40.705],
+                  zoom: 12,
+                  speed: 1
+              });
+          }
+      }).catch(console.error);
 };
 
 restaurantButton.onclick = () => {
   var restaurants = document.getElementById('restaurants-choices');
   var selection = restaurants.options[restaurants.selectedIndex].value;
   console.log('restaurants: ', selection);
+  fetch('/api/restaurants/' + selection)
+      .then(result => {
+          return result.json();
+      }).then(data => {
+          console.log(data);
+          //Add to itinerary
+          const parent = document.getElementById(itineraryIds[1]);
+          let divNode = document.createElement('DIV');
+          let pNode = document.createElement('SPAN');
+          pNode.innerHTML = data.name;
+          divNode.appendChild(pNode);
+          let remNode = removeButton.cloneNode(true);
+          divNode.appendChild(remNode);
+          parent.appendChild(divNode);
+          //Add marker
+          var newMarker = buildMarker('restaurants', data.place.location);
+          newMarker.addTo(map);
+          map.flyTo({
+              center: data.place.location,
+              zoom: 15,
+              curve: 1.8
+          });
+          remNode.onclick = function () {
+              newMarker.remove();
+              this.parentElement.remove();
+              map.flyTo({
+                  center: [-74.009, 40.705],
+                  zoom: 12,
+                  speed: 1
+              });
+          }
+      }).catch(console.error);
 };
 
 activityButton.onclick = () => {
   var activities = document.getElementById('activities-choices');
   var selection = activities.options[activities.selectedIndex].value;
   console.log('activities: ', selection);
+  fetch('/api/activities/' + selection)
+      .then(result => {
+          return result.json();
+      }).then(data => {
+          console.log(data);
+          //Add to itinerary
+          const parent = document.getElementById(itineraryIds[2]);
+          let divNode = document.createElement('DIV');
+          let pNode = document.createElement('SPAN');
+          pNode.innerHTML = data.name;
+          divNode.appendChild(pNode);
+          let remNode = removeButton.cloneNode(true);
+          divNode.appendChild(remNode);
+          parent.appendChild(divNode);
+          //Add marker
+          var newMarker = buildMarker('activities', data.place.location);
+          newMarker.addTo(map);
+          map.flyTo({
+              center: data.place.location,
+              zoom: 15,
+              curve: 1.8
+          });
+          remNode.onclick = function () {
+              newMarker.remove();
+              this.parentElement.remove();
+              map.flyTo({
+                  center: [-74.009, 40.705],
+                  zoom: 12,
+                  speed: 1
+              });
+          }
+      }).catch(console.error);
 };
+
+
 
 // array of three arrays hotels, activities, and restaurants (in that order)
 // select ids are hotels-choices, restaurants-choices, activities-choices
--- server/api.js
+++ server/api.js
@@ -3,33 +3,50 @@ const router = express.Router();
 
 const {db, Hotel, Place, Activity, Restaurant} = require("./models");
 
-
 router.get('/attractions', (req, res, next) => {
   Promise.all([Hotel.findAll({include: [Place]}), Restaurant.findAll({include: [Place]}), Activity.findAll({include: [Place]})])
   .then( arr => res.status(200).json(arr))
   .catch(next);
 });
 
-router.get('/hotels', (req, res, next) => {
-  Hotel.findAll()
-  .then( hotels => {
-    res.status(200).json(hotels);
+router.get('/hotels/:id', (req, res, next) => {
+    var pid = req.params.id;
+    Hotel.findOne({
+        where: {
+            id: pid
+        },
+        include: [Place]
+    })
+  .then( hotel => {
+    res.status(200).json(hotel);
   })
   .catch(next);
 });
 
-router.get('/activities', (req, res, next) => {
-  Activity.findAll()
-  .then( activities => {
-    res.status(200).json(activities);
+router.get('/activities/:id', (req, res, next) => {
+    var pid = req.params.id;
+    Activity.findOne({
+        where: {
+            id: pid
+        },
+        include: [Place]
+    })
+  .then( activity => {
+    res.status(200).json(activity);
   })
   .catch(next);
 });
 
-router.get('/restaurants', (req, res, next) => {
-  Restaurant.findAll()
-  .then( restaurants => {
-    res.status(200).json(restaurants);
+router.get('/restaurants/:id', (req, res, next) => {
+    var pid = req.params.id;
+    Restaurant.findOne({
+        where: {
+            id: pid
+        },
+        include: [Place]
+    })
+  .then( restaurant => {
+    res.status(200).json(restaurant);
   })
   .catch(next);
 });
